fa7c87011430deb1668dea1a6d464f1925f2b554
Ahmed Hesham <ahmedhesham11m@gmail.com>
2025-07-18 10:39:20 +0300

    added basic structure for swagger

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDoc = YAML.load("./api.yaml");
 const connectDB = require("./config/db");
 const bookRoutes = require("./routes/book.route");
-
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+connectDB();
 
-connectDB(); //database connection
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc)); // Serve Swagger UI
 
 app.use("/book", bookRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
