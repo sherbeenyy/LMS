511f4974be0bdfafded0486ad7f9d67307b741d0
Ahmed Hesham <ahmedhesham11m@gmail.com>
2025-07-18 19:08:08 +0300

    get book by id

--- routes/book.route.js
+++ routes/book.route.js
@@ -6,6 +6,10 @@ const Book = require("../models/book.model");
 const { addBookValidator } = require("../validators/bookValidator");
 const validateRequest = require("../middlewares/validateRequests");
 
+//POST
+//books/add
+//Adds new book to the db
+//A message of success
 router.post("/add", addBookValidator, validateRequest, async (req, res) => {
   try {
     const { title, author, isbn, copiesInStock, price } = req.body;
@@ -40,7 +44,10 @@ router.post("/add", addBookValidator, validateRequest, async (req, res) => {
     });
   }
 });
-
+//GET
+//books/all
+//Fetches all books from the db
+//Returns an array of books
 router.get("/all", async (req, res) => {
   try {
     const books = await Book.find({});
@@ -50,9 +57,46 @@ router.get("/all", async (req, res) => {
         .json({ status: false, message: "No books found." });
     }
     res.status(200).json(books);
-  } catch (error) {
-    console.error("Error fetching books:", error);
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ message: "Server error while fetching books." });
+  }
+});
+
+//GET
+//books/:id
+//Fetches a book by its ID
+//Returns the book object
+const mongoose = require("mongoose");
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  // if the object ID is not valid, return 404 not the actual error for security
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({
+      status: false,
+      message: "Book not found.",
+    });
+  }
+
+  try {
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({
+        status: false,
+        message: "Book not found.",
+      });
+    }
+
+    res.status(200).json(book);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Server error while fetching book.",
+      error: err.message,
+    });
   }
 });
 
