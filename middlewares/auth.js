8deb387f97228dae1d23dc28cdc83cd369ac174f
Ahmed Hesham <ahmedhesham11m@gmail.com>
2025-08-05 21:58:19 +0300

    auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+// middlewares/auth.js
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ status: false, message: "Access denied. No token provided." });
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res
+      .status(403)
+      .json({ status: false, message: "Invalid or expired token." });
+  }
+};
+
+module.exports = authMiddleware;
